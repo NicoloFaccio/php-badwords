b0aec1559d409f865858446c2f1a7a7e7f36da58
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-02-23 17:09:22 +0100

    created the tag form

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-echo "ciao";
+// str_replace(porzioneDaModificare, conCosa, stringa);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>intro php</title>
+</head>
+<body>
+    <form action="saluto.php" method="post">
+        <input type="text" name="words">
+        <button type="submit">Invia</button>
+    </form>
+</body>
+</html>
