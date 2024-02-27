675c29208321d635925d7c068180344fca8f1c4b
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-02-27 15:30:06 +0100

    recreated the file index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 // str_replace(porzioneDaModificare, conCosa, stringa);
+
+
+$string = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem assumenda, commodi aliquam, delectus ipsa sapiente asperiores ab expedita maiores dolores perferendis et facere fugiat, quam beatae distinctio quae! Aspernatur, possimus? '
 ?>
 
 <!DOCTYPE html>
@@ -11,7 +14,8 @@
 </head>
 <body>
     <form action="saluto.php" method="post">
-        <input type="text" name="words">
+        <input type="text" placeholder='sscrivere una parola' name="words">
+        <textarea name="paragraph" id="" cols="30" rows="10"></textarea>
         <button type="submit">Invia</button>
     </form>
 </body>
